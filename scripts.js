d6fadef70d3a374fd65d7ad390c01c72d39debca
Renee <156127959+reneexie01@users.noreply.github.com>
2024-06-19 06:01:18 +1000

    Added console playGame functionality without win conditions

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
 // gameboard module
 
 const gameboard = (function Gameboard() {
-    const gameboard = [];
+    const board = ["","","","","","","","",""];
 
-    const renderGameboard = () => gameboard;
+    const renderGameboard = () => board;
 
-    return { renderGameboard };
+    const addMark = function(index, mark) {
+        if (board[index] === "") {
+            board[index] = mark;
+        } else {
+            console.log(`Invalid move. Try again.`)
+            return;
+        }
+    }
+
+    return { renderGameboard, addMark };
 })();
 
 // person factory function
@@ -14,15 +23,31 @@ function createPerson(name, mark) {
     return { name, mark };
 }
 
+let playCount = 0 // must address this global variable
+
 // displayController module
 
 const displayController = (function displayController() {
     const player1 = createPerson("Jack", "X");
     const player2 = createPerson("Jill", "O");
 
+    const playGame = function(index) {
+        if (playCount === 0 || playCount % 2 === 0) {
+            gameboard.addMark(index, player1.mark);
+            playCount++;
+            console.log(gameboard.renderGameboard());
+        } else if (playCount % 2 !== 0) {
+            gameboard.addMark(index, player2.mark);
+            playCount++;
+            console.log(gameboard.renderGameboard());
+        }
+    }
 
-    return { player1, player2 };
+    return { player1, player2, playGame };
 })()
 
 console.log(gameboard.renderGameboard());
-console.log(displayController.player1);
+console.log(displayController.playGame(1));
+console.log(displayController.playGame(2));
+console.log(displayController.playGame(0));
+console.log(displayController.playGame(0));
